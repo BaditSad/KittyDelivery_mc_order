54d1b5c3f6fbec36ba804357002d763f706945c9
BaditSad <bdumortier0@gmail.com>
2024-06-14 12:58:49 +0200

    docker init env

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config(); // Charger les variables d'environnement à partir de .env
+
 const ordersRouter = require("./controllers/OrderController");
 const deliveriesRouter = require("./controllers/DeliveryController");
 const restaurantsRouter = require("./controllers/RestaurantController");
@@ -5,29 +7,33 @@ const usersRouter = require("./controllers/UserController");
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
 const app = express();
-const port = 3004;
+const port = process.env.PORT || 3004;
+const mongoURI = process.env.MONGO_URI;
 
 app.use(cors());
-
-const db = require("./models");
-db.mongoose
-  .connect(db.url)
-  .then(() => {
-    console.log("Connected to the database!");
-  })
-  .catch((err) => {
-    console.log("Cannot connect to the database!", err);
-    process.exit();
-  });
-
-//Ici on envoit les infos vers le front
-
 app.use(bodyParser.json());
 
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log("Connected to MongoDB!");
+})
+.catch((err) => {
+  console.error("Error connecting to MongoDB:", err);
+  process.exit();
+});
+
+// Routes vers les différents contrôleurs
 app.use("/orders", ordersRouter);
 app.use("/deliveries", deliveriesRouter);
 app.use("/user", usersRouter);
 app.use("/restaurant", restaurantsRouter);
 
-app.listen(port, () => console.log(`app running on http://localhost:${port}`));
+app.listen(port, () => {
+  console.log(`App running on http://localhost:${port}`);
+});
